0d8b6eb47e3264129050c7d39498a839f473547f
Hari Amoor <amoor.hari@gmail.com>
2020-04-24 04:20:38 -0400

    Revert "fix indentation"
    This reverts commit 10997288e6e1e528878a5e5fc8f74830b420341b.

--- src/cli.rs
+++ src/cli.rs
@@ -3,25 +3,25 @@ use clap::clap_app;
 
 pub fn parse() -> Option<Instruction<String>> {
     let matches = clap_app!(todo_cli =>
-        (version: "0.1")
-        (author: "USACS at Rutgers University <usacs.rutgers.edu>")
-        (about: "Simple to-do list CLI in Rust")
-        (@subcommand print =>
-            (about: "Print out all values stored in CLI")
-        )
-        (@subcommand add =>
-            (@arg NEW: +required +takes_value "Task to add")
-            (about: "Add a task to CLI")
-        )
-        (@subcommand rm =>
-            (@arg NUM: +required +takes_value "Identifier of task to remove")
-            (about: "Remove a task from CLI")
-        )
-        (@subcommand modify =>
-            (@arg NUM: +required +takes_value "Identifier of task to modify")
-            (@arg NEW: -n --new +required +takes_value "Task number to modify")
-            (about: "Modify a task stored by the CLI")
-        )
+                            (version: "0.1")
+                            (author: "USACS at Rutgers University <usacs.rutgers.edu>")
+                            (about: "Simple to-do list CLI in Rust")
+                            (@subcommand print =>
+                             (about: "Print out all values stored in CLI")
+                            )
+                            (@subcommand add =>
+                             (@arg NEW: +required +takes_value "Task to add")
+                             (about: "Add a task to CLI")
+                            )
+                            (@subcommand rm =>
+                             (@arg NUM: +required +takes_value "Identifier of task to remove")
+                             (about: "Remove a task from CLI")
+                            )
+                            (@subcommand modify =>
+                             (@arg NUM: +required +takes_value "Identifier of task to modify")
+                             (@arg NEW: -n --new +required +takes_value "Task number to modify")
+                             (about: "Modify a task stored by the CLI")
+                            )
     )
     .get_matches();
 
@@ -29,9 +29,11 @@ pub fn parse() -> Option<Instruction<String>> {
         return Some(Instruction::Add(matches.value_of("NEW")?.to_string()));
     } else if let Some(matches) = matches.subcommand_matches("rm") {
         // Why can't we use a `?` after the parse?
-        return Some(Instruction::Remove(
-            matches.value_of("NUM")?.parse().unwrap(),
-        ));
+        return if let Ok(idx) = matches.value_of("NUM")?.parse() {
+            Some(Instruction::Remove(idx))
+        } else {
+            None
+        };
     } else if let Some(matches) = matches.subcommand_matches("modify") {
         return Some(Instruction::Modify(
             // This code might panic. Why? Exercise(Week 1): gracefully handle
